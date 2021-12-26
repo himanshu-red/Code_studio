90d423a2787208e7259145248fb3bd9302be8e61
himanshu-red <45516142+himanshu-red@users.noreply.github.com>
2021-12-26 07:00:56 +0530

    Create printLikeAWave.cpp

---
+++ printLikeAWave.cpp
@@ -0,0 +1,21 @@
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+   bool reversed = false; 
+    vector<int> v; 
+    for (int i = 0; i< mCols; i++)
+    {
+            for (int j = 0; j<nRows; j++)
+            {
+                if(!(reversed))
+                	v.push_back(arr[j][i]); 
+                else 
+					v.push_back(arr[nRows-j-1][i]) ; 
+            }
+        if (reversed)
+            reversed = false; 
+        else 
+            reversed = true; 
+	}
+        return v; 
+}
